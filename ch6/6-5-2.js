fffb744ae8ed0c746d3131bca5bee53b6d241494
tony <chcjswo@gmail.com>
2022-07-24 23:58:55 +0900

    함수 선언 바꾸기

--- ch6/6-5-2.js
+++ ch6/6-5-2.js
@@ -1,16 +1,14 @@
 export default class Book {
-  #reservations;
-  constructor() {
-    this.#reservations = [];
-  }
+	#reservations;
+	constructor() {
+		this.#reservations = [];
+	}
 
-  addReservation(customer) {
-    this.#reservations.push(customer);
-  }
+	addReservation(customer, isPriority = false) {
+		this.#reservations.push(customer);
+	}
 
-  hasReservation(customer) {
-    return this.#reservations.some(
-      (reservedCustomer) => reservedCustomer.id === customer.id
-    );
-  }
+	hasReservation(customer) {
+		return this.#reservations.some((reservedCustomer) => reservedCustomer.id === customer.id);
+	}
 }
