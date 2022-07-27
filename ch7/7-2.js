4a4bc0363b4f58cc087f751a3977fee53b669b59
tony <chcjswo@gmail.com>
2022-07-27 23:14:23 +0900

    컬렉션 캡슐화 하기

--- ch7/7-2.js
+++ ch7/7-2.js
@@ -1,41 +1,61 @@
 export class Person {
-  #name;
-  #courses;
-  constructor(name) {
-    this.#name = name;
-    this.#courses = [];
-  }
+	#name;
+	#courses;
+	constructor(name) {
+		this.#name = name;
+		this.#courses = [];
+	}
 
-  get name() {
-    return this.#name;
-  }
+	get name() {
+		return this.#name;
+	}
 
-  get courses() {
-    return this.#courses;
-  }
+	get courses() {
+		return [...this.#courses];
+	}
 
-  set courses(courses) {
-    this.#courses = courses;
-  }
+	addCources(course) {
+		this.#courses.push(course);
+	}
+
+	removeCourse(course, runIfAbsend) {
+		const index = this.#courses.indexOf(course);
+		if (index === -1) {
+			runIfAbsend();
+			return;
+		}
+		this.#courses.splice(index, 1);
+	}
 }
 
 export class Course {
-  #name;
-  #isAdvanced;
-  constructor(name, isAdvanced) {
-    this.#name = name;
-    this.#isAdvanced = isAdvanced;
-  }
+	#name;
+	#isAdvanced;
+	constructor(name, isAdvanced) {
+		this.#name = name;
+		this.#isAdvanced = isAdvanced;
+	}
 
-  get name() {
-    return this.#name;
-  }
+	get name() {
+		return this.#name;
+	}
 
-  get isAdvanced() {
-    return this.#isAdvanced;
-  }
+	get isAdvanced() {
+		return this.#isAdvanced;
+	}
 }
 
 const ellie = new Person('엘리');
-ellie.courses.push(new Course('리팩토링', true));
+const c1 = new Course('리팩토링1', true);
+const c2 = new Course('리팩토링2', true);
+ellie.addCources(c1);
+ellie.addCources(c2);
+console.log(ellie.courses.length);
+ellie.removeCourse(c1, () => {
+	console.log('해당 강좌는 없습니다.');
+});
+console.log(ellie.courses.length);
+ellie.removeCourse(c1, () => {
+	console.log('해당 강좌는 없습니다.');
+});
 console.log(ellie.courses.length);
