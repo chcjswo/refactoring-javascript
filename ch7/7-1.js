8270aec92cb3bacdf39924d80e28ae43825a4a0e
tony <chcjswo@gmail.com>
2022-07-27 23:03:09 +0900

    레코드 캡슐화 하기

--- ch7/7-1.js
+++ ch7/7-1.js
@@ -1,4 +1,28 @@
-const organization = { name: 'Acme Gooseberries', country: 'GB' };
+class Organization {
+	#name;
+	#country;
+	constructor(data) {
+		this.#name = data.name;
+		this.#country = data.country;
+	}
+
+	get name() {
+		return this.#name;
+	}
+
+	set name(value) {
+		this.#name = value;
+	}
+
+	get country() {
+		return this.#country;
+	}
+
+	set country(value) {
+		this.#country = value;
+	}
+}
+const organization = new Organization({ name: 'Acme Gooseberries', country: 'GB' });
 
 organization.name = 'Dream Coding';
 console.log(organization.name);
