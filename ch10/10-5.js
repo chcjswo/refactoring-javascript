ddbc12e44511d0504b1ed4ef5c90ba5632e92010
tony <chcjswo@gmail.com>
2022-08-04 23:45:49 +0900

    특이케이스 추가하기

--- ch10/10-5.js
+++ ch10/10-5.js
@@ -1,42 +1,45 @@
 export class Site {
-  constructor(customer) {
-    this._customer = customer;
-  }
+	constructor(customer) {
+		this._customer = customer;
+	}
 
-  get customer() {
-    return this._customer;
-  }
+	get customer() {
+		return this._customer === 'unknown' ? new UnknownCustomer() : new Customer();
+	}
 }
 
 export class Customer {
-  constructor(name) {
-    this._name = name;
-  }
+	constructor(name) {
+		this._name = name;
+	}
 
-  get name() {
-    return this._name;
-  }
+	get name() {
+		return this._name;
+	}
 
-  get billingPlan() {
-    //
-  }
+	get billingPlan() {
+		//
+	}
 
-  set billingPlan(arg) {
-    //
-  }
+	set billingPlan(arg) {
+		//
+	}
 
-  get paymentHistory() {
-    //
-  }
+	get paymentHistory() {
+		//
+	}
+}
+
+class UnknownCustomer extends Customer {
+	get name() {
+		return 'occupant';
+	}
 }
 
 // 사용하는 부분
 export function customerName(site) {
-  const aCustomer = site.customer;
-  // 더 많은 코드가 여기에 있음
-  let customerName;
-  if (aCustomer === 'unknown') customerName = 'occupant';
-  else customerName = aCustomer.name;
+	const aCustomer = site.customer;
+	customerName = aCustomer.name;
 
-  return customerName;
+	return customerName;
 }
