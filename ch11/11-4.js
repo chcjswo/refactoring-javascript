217375953e6b42b7358ff15049cfb273a9379df1
tony <chcjswo@gmail.com>
2022-08-05 23:56:13 +0900

    객체 통째로 넘기기

--- ch11/11-4.js
+++ ch11/11-4.js
@@ -1,22 +1,18 @@
 export function temperatureAlerts(room, plan) {
-  const alerts = [];
-  const low = room.daysTempRange.low;
-  const high = room.daysTempRange.high;
-  if (!plan.withinRange(low, high)) {
-    alerts.push('room temperature went outside range');
-  }
+	const alerts = [];
+	if (!plan.withinRange(room.daysTempRange)) {
+		alerts.push('room temperature went outside range');
+	}
 
-  return alerts;
+	return alerts;
 }
 
 export class HeatingPlan {
-  constructor(temperatureRange) {
-    this._temperatureRange = temperatureRange;
-  }
+	constructor(temperatureRange) {
+		this._temperatureRange = temperatureRange;
+	}
 
-  withinRange(bottom, top) {
-    return (
-      bottom >= this._temperatureRange.low && top <= this._temperatureRange.high
-    );
-  }
+	withinRange(range) {
+		return range.bottom >= this._temperatureRange.low && range.top <= this._temperatureRange.high;
+	}
 }
