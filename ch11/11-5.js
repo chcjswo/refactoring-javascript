a7452f6ccd13ca11181f06cd13eee83edcd006d7
tony <chcjswo@gmail.com>
2022-08-05 23:59:04 +0900

    매개변수를 질의 함수로 바꾸기

--- ch11/11-5.js
+++ ch11/11-5.js
@@ -1,23 +1,27 @@
 export class Order {
-  constructor(quantity, itemPrice) {
-    this.quantity = quantity;
-    this.itemPrice = itemPrice;
-  }
+	constructor(quantity, itemPrice) {
+		this.quantity = quantity;
+		this.itemPrice = itemPrice;
+	}
 
-  get finalPrice() {
-    const basePrice = this.quantity * this.itemPrice;
-    let discountLevel;
-    if (this.quantity > 100) discountLevel = 2;
-    else discountLevel = 1;
-    return this.discountedPrice(basePrice, discountLevel);
-  }
+	get basePrice() {
+		return this.quantity * this.itemPrice;
+	}
 
-  discountedPrice(basePrice, discountLevel) {
-    switch (discountLevel) {
-      case 1:
-        return basePrice * 0.95;
-      case 2:
-        return basePrice * 0.9;
-    }
-  }
+	get discountLevel() {
+		return this.quantity > 100 ? 2 : 1;
+	}
+
+	get finalPrice() {
+		return this.discountedPrice();
+	}
+
+	discountedPrice() {
+		switch (this.discountLevel) {
+			case 1:
+				return this.basePrice * 0.95;
+			case 2:
+				return this.basePrice * 0.9;
+		}
+	}
 }
