9aaab383310a949ebd5f4d0331b7a808d3606c89
tony <chcjswo@gmail.com>
2022-08-07 00:08:15 +0900

    타입코드를 서브클래스로 바꾸기

--- ch12/12-6.js
+++ ch12/12-6.js
@@ -1,26 +1,41 @@
 class Employee {
-  #name;
-  #type;
-  constructor(name, type) {
-    this.validateType(type);
-    this.#name = name;
-    this.#type = type;
-  }
+	#name;
+	constructor(name) {
+		this.#name = name;
+	}
 
-  validateType(arg) {
-    if (!['engineer', 'manager', 'salesperson'].includes(arg)) {
-      throw new Error(`${arg}라는 직원 유형은 없습니다.`);
-    }
-  }
+	get type() {
+		return 'employee';
+	}
 
-  get type() {
-    return this.#type;
-  }
-
-  toString() {
-    return `${this.#name} (${this.type})`;
-  }
+	toString() {
+		return `${this.#name} (${this.type})`;
+	}
 }
 
-const ellie = new Employee('엘리', 'engineer');
-const bob = new Employee('밥', 'manager');
+class Engineer extends Employee {
+	constructor(name) {
+		super(name);
+	}
+
+	get type() {
+		return 'engineer';
+	}
+}
+
+class Manager extends Employee {
+	constructor(name) {
+		super(name);
+	}
+
+	get type() {
+		return 'manager';
+	}
+}
+
+const ellie = new Engineer('엘리', 'engineer');
+const bob = new Manager('밥', 'manager');
+
+console.log(ellie.toString());
+console.log(bob.toString());
+
