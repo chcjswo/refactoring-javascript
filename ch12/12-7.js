645ca1e23d5213a3ed0a15ebc8a88f73cf5f2f17
tony <chcjswo@gmail.com>
2022-08-07 00:16:39 +0900

    서브 클래스 제거하기

--- ch12/12-7.js
+++ ch12/12-7.js
@@ -1,53 +1,47 @@
 class Person {
-  #name;
-  constructor(name) {
-    this.#name = name;
-  }
+	#name;
+	#genderCode;
+	constructor(name, genderCode) {
+		this.#name = name;
+		this.#genderCode = genderCode;
+	}
 
-  get name() {
-    return this.#name;
-  }
+	get name() {
+		return this.#name;
+	}
 
-  get genderCode() {
-    return 'X';
-  }
-}
+	get genderCode() {
+		return this.#genderCode;
+	}
 
-class Male extends Person {
-  get genderCode() {
-    return 'M';
-  }
-}
+	get isMale() {
+		return this.#genderCode === 'M';
+	}
 
-class Female extends Person {
-  get genderCode() {
-    return 'F';
-  }
+	static create(record) {
+		switch (record.gender) {
+			case 'M':
+				return new Person(record.name, 'M');
+			case 'F':
+				return new Person(record.name, 'F');
+			default:
+				return new Person(record.name, 'X');
+		}
+	}
 }
 
 function loadFromInput(data) {
-  const result = [];
-  data.forEach((record) => {
-    let person;
-    switch (record.gender) {
-      case 'M':
-        person = new Male(record.name);
-        break;
-      case 'F':
-        person = new Female(record.name);
-        break;
-      default:
-        person = new Person(record.name);
-    }
-    result.push(person);
-  });
-  return result;
+	const result = [];
+	data.forEach((record) => {
+		result.push(Person.create(record));
+	});
+	return result;
 }
 
 const people = loadFromInput([
-  { name: '엘리', gender: 'F' },
-  { name: '철수', gender: 'M' },
-  { name: '밥', gender: 'M' },
+	{ name: '엘리', gender: 'F' },
+	{ name: '철수', gender: 'M' },
+	{ name: '밥', gender: 'M' },
 ]);
-const numberOfMales = people.filter((p) => p instanceof Male).length;
+const numberOfMales = people.filter((p) => p.isMale).length;
 console.log(numberOfMales);
